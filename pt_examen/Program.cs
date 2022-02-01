65d2a5a0e017125b5ddd8c85780decc0a37d4540
Iosif Turcu <iosif.turcu@emmanuel.ro>
2022-02-01 16:51:33 +0200

    initial setup

---
+++ pt_examen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pt_examen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
